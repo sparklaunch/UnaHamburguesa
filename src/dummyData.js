35bdd92ee7acef0cba94296f8d4058e512bcc0b3
Jinwook Kim <34114794+sparklaunch@users.noreply.github.com>
2021-11-25 18:44:24 +0900

    Add dummy data

---
+++ src/dummyData.js
@@ -0,0 +1,88 @@
+const burgers = [
+  {
+    id: 0,
+    nameKor: "스리라차 버거",
+    nameEng: "Sriracha",
+    price: 5400
+  },
+  {
+    id: 1,
+    nameKor: "메가바이트 버거",
+    nameEng: "Megabite",
+    price: 5400
+  },
+  {
+    id: 2,
+    nameKor: "산체스 버거",
+    nameEng: "Sanchez",
+    price: 5400
+  },
+  {
+    id: 3,
+    nameKor: "그릴드 불고기 버거",
+    nameEng: "GrilledBulgogi",
+    price: 5400
+  },
+  {
+    id: 4,
+    nameKor: "미트매니아 버거",
+    nameEng: "MeatMania",
+    price: 5400
+  },
+  {
+    id: 5,
+    nameKor: "스모키 살사 버거",
+    nameEng: "SmokySalsa",
+    price: 5400
+  },
+  {
+    id: 6,
+    nameKor: "시그니처 버거",
+    nameEng: "Signature",
+    price: 5400
+  },
+  {
+    id: 7,
+    nameKor: "어메이징 버거",
+    nameEng: "Amazing",
+    price: 5400
+  },
+  {
+    id: 8,
+    nameKor: "오리지널 버거",
+    nameEng: "Original",
+    price: 5400
+  },
+  {
+    id: 9,
+    nameKor: "데리마요 버거",
+    nameEng: "Derimayo",
+    price: 5400
+  },
+  {
+    id: 10,
+    nameKor: "코울슬로 버거",
+    nameEng: "Coleslaw",
+    price: 5400
+  },
+  {
+    id: 11,
+    nameKor: "페퍼로니 치킨 버거",
+    nameEng: "PepperoniChicken",
+    price: 5400
+  },
+  {
+    id: 12,
+    nameKor: "페퍼로니 버거",
+    nameEng: "Pepperoni",
+    price: 5400
+  },
+  {
+    id: 13,
+    nameKor: "갈릭 앤 갈릭 버거",
+    nameEng: "GarlicAndGarlic",
+    price: 5400
+  }
+];
+
+export default burgers;
