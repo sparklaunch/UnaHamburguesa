d28161624f9318535d63c412ecee39e098f1a4ec
Jinwook Kim <34114794+sparklaunch@users.noreply.github.com>
2021-11-28 12:51:37 +0900

    Realize prices

--- src/dummyData.js
+++ src/dummyData.js
@@ -9,79 +9,79 @@ const burgers = [
     id: 1,
     nameKor: "메가바이트 버거",
     nameEng: "Megabite",
-    price: 5400
+    price: 7200
   },
   {
     id: 2,
     nameKor: "산체스 버거",
     nameEng: "Sanchez",
-    price: 5400
+    price: 6600
   },
   {
     id: 3,
     nameKor: "그릴드 불고기 버거",
     nameEng: "GrilledBulgogi",
-    price: 5400
+    price: 5700
   },
   {
     id: 4,
     nameKor: "미트매니아 버거",
     nameEng: "MeatMania",
-    price: 5400
+    price: 5100
   },
   {
     id: 5,
     nameKor: "스모키 살사 버거",
     nameEng: "SmokySalsa",
-    price: 5400
+    price: 5500
   },
   {
     id: 6,
     nameKor: "시그니처 버거",
     nameEng: "Signature",
-    price: 5400
+    price: 8800
   },
   {
     id: 7,
     nameKor: "어메이징 버거",
     nameEng: "Amazing",
-    price: 5400
+    price: 9600
   },
   {
     id: 8,
     nameKor: "오리지널 버거",
     nameEng: "Original",
-    price: 5400
+    price: 4800
   },
   {
     id: 9,
     nameKor: "데리마요 버거",
     nameEng: "Derimayo",
-    price: 5400
+    price: 3600
   },
   {
     id: 10,
     nameKor: "코울슬로 버거",
     nameEng: "Coleslaw",
-    price: 5400
+    price: 5000
   },
   {
     id: 11,
     nameKor: "페퍼로니 치킨 버거",
     nameEng: "PepperoniChicken",
-    price: 5400
+    price: 6700
   },
   {
     id: 12,
     nameKor: "페퍼로니 버거",
     nameEng: "Pepperoni",
-    price: 5400
+    price: 5900
   },
   {
     id: 13,
     nameKor: "갈릭 앤 갈릭 버거",
     nameEng: "GarlicAndGarlic",
-    price: 5400
+    price: 4800
   }
 ];
 
