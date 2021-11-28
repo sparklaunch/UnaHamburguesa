932e7b8e06e737cb6654fd3b36b3c1c390615026
Jinwook Kim <34114794+sparklaunch@users.noreply.github.com>
2021-11-28 12:49:20 +0900

    Add remove item from orders feature

--- src/components/OrderListItem.js
+++ src/components/OrderListItem.js
@@ -1,14 +1,28 @@
 import classes from "./OrderListItem.module.css";
 import burgers from "../dummyData";
+import { useContext } from "react";
+import { BurgerContext } from "../store/BurgerContext";
 
 function OrderListItem({ name, quantity }) {
+  const { selectedBurgers, setSelectedBurgers } = useContext(BurgerContext);
   const theBurger = burgers.find((burger) => burger.nameEng === name);
+  function removeHandler() {
+    const target = theBurger.nameEng;
+    setSelectedBurgers((previous) => {
+      const copy = { ...previous };
+      delete copy[target];
+      return copy;
+    });
+  }
   return (
     <div className={classes.orderListItem}>
       <p>{theBurger.nameKor}</p>
       <div className={classes.right}>
         <p>KRW {theBurger.price.toLocaleString()}</p>
         <p>{quantity} EA</p>
+        <p className={classes.removeButton} onClick={removeHandler}>
+          &times;
+        </p>
       </div>
     </div>
   );
